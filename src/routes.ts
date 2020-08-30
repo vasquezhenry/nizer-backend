480a6641145743c2a46b3923368c1890a2207e14
Henry Vasquez <henry@Henrys-MacBook-Pro.local>
2020-08-30 04:51:15 -0400

    Connect router to individual modules

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import budgets from "./modules/budgets";
-import {Application} from "express"
+import accounts from "./modules/accounts";
+import categories from "./modules/categories";
+import subCategories from "./modules/sub_categories";
+import transactions from "./modules/transactions";
+import { Application } from "express";
 
-export default (app:Application) => {
+export default (app: Application) => {
   const router = Router();
 
   budgets(router);
+  accounts(router);
+  categories(router);
+  subCategories(router);
+  transactions(router);
 
-  app.use("/api",router)
+  app.use("/api", router);
   return app;
 };
