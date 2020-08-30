1a4fe271f64a7cffb8574e28da9a058f04d5539f
Henry Vasquez <henry@Henrys-MacBook-Pro.local>
2020-08-29 22:59:56 -0400

    add example env file

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cors from "cors";
 
-import router from "./routes"
+import router from "./routes";
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
+app.use(cors());
 
 router(app);
-
 
 export default app;
